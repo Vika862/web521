70c7f9b3a3330a55364ecb302fb7588fbb7039ea
Vika.Kostyreva <cos.v2002@yandex.com>
2025-10-05 18:49:49 +0300

    task2

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
   <?php
   //task1
   $arrayOne = [1,2,3,4];
+
   $doubleArray = array_map(function($number){
-    return $arrayOne*2;
+    return $number*2;
   },$arrayOne);
+
+ echo implode(",", $doubleArray),"</br>";
+
+ //2
+$fruits = ['apple','banana', 'kiwi', 'strawberry','watermelow','orange'];
+
+$fruitsB = array_filter($fruits, function($fruit){
+  return strlen($fruit)>5;
+});
+
+echo implode(",", $fruitsB), "</br>";
